dbd0e56b5392f8e0d5913c47199dbf79c3e64b94
Roberto Araujo <roberto.andrioli@gmail.com>
2021-07-03 13:52:29 -0300

    Update PharoLauncher.app to version 2.2

--- Casks/pharo-launcher.rb
+++ Casks/pharo-launcher.rb
@@ -1,6 +1,6 @@
 cask 'pharo-launcher' do
-  version '2.0'
-  sha256 '1da8ef2752b359b804072ba692990e989be4c66d6a761a50ab0693082ac805c7'
+  version '2.2'
+  sha256 '99edeafeeaee64958ef14287791df2228c1d5c8ad83732452d27b471c35dac52'
 
   url "https://files.pharo.org/pharo-launcher/#{version}/PharoLauncher-#{version}-x64.dmg"
   name 'Pharo Launcher'
