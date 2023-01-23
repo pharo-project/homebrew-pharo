4ca97ae2f03b8fcf9236a6332b01ee1bc3386d83
Noah Friedman <noahfriedman2@gmail.com>
2023-01-23 10:42:23 -0500

    Add configuration for Apple Silicon (ARM64) downloads on compatible systems

--- Casks/pharo-launcher.rb
+++ Casks/pharo-launcher.rb
@@ -1,10 +1,15 @@
-cask 'pharo-launcher' do
-  version '2.2'
-  sha256 '99edeafeeaee64958ef14287791df2228c1d5c8ad83732452d27b471c35dac52'
+cask "pharo-launcher" do
+  arch arm: "arm64",
+       intel: "x64"
 
-  url "https://files.pharo.org/pharo-launcher/#{version}/PharoLauncher-#{version}-x64.dmg"
-  name 'Pharo Launcher'
-  homepage 'http://pharo.org/download'
+  version "3.0.1"
+  sha256 arm: "f3651f285c548a6ecc501a6f5c268c6f644d143b128c43c9395a02fba590e930",
+         intel: "44b4c46f00a381e8b3d92838ba7ea7615cb4ff66490a4e7c53675fbe38d8e226"
 
-  app 'PharoLauncher.app'
+  url "https://files.pharo.org/pharo-launcher/#{version}/PharoLauncher-#{version}-#{arch}.dmg"
+  name "Pharo Launcher"
+  desc "The fastest way to get a working Pharo environment."
+  homepage "http://pharo.org/download"
+
+  app "PharoLauncher.app"
 end
